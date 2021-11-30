a5abc67a4d51751fe2ef618a1025ec8800744f65
Alvaro Avila <aavila@codeforvenezuela.org>
2021-11-29 23:49:07 -0400

    finished block explorer

--- v1/utils/block_explorer.py
+++ v1/utils/block_explorer.py
@@ -1,11 +1,14 @@
-from socket import socket
+from argparse import ArgumentParser
+from socket import socket, AF_INET, SOCK_DGRAM
 from json import loads
 from gnupg import GPG
 from execution.semantic.block import Block, create_block_from_json
 from execution.utils import Event, send_message_to_node, BUFSIZE
 
 
-def parse_response(response: bytes) -> Block:
+def explore(data: dict, height: int, name: str, port: int, sock: socket, gpg: GPG) -> Block:
+    send_message_to_node(data, Event.BLOCK_EXPLORE.value, name, port, sock, gpg)
+    response = sock.recv(BUFSIZE)
     response_json = loads(response.decode())
     data = response_json["data"]
     block_json = data["block"]
@@ -14,13 +17,26 @@ def parse_response(response: bytes) -> Block:
 
 def explore_by_height(height: int, node_name: str, node_port: int, sock: socket, gpg: GPG) -> Block:
     data = {"height": height}
-    send_message_to_node(data, Event.BLOCK_EXPLORE.value, node_name, node_port, sock, gpg)
-    response = sock.recv(BUFSIZE)
-    return parse_response(response)
+    return explore(data, Event.BLOCK_EXPLORE.value, node_name, node_port, sock, gpg)
 
 
 def explore_by_hash(_hash: str, node_name: str, node_port: int, sock: socket, gpg: GPG) -> Block:
     data = {"hash": _hash}
-    send_message_to_node(data, Event.BLOCK_EXPLORE.value, node_name, node_port, sock, gpg)
-    response = sock.recv(BUFSIZE)
-    return parse_response(response)
+    return explore(data, Event.BLOCK_EXPLORE.value, node_name, node_port, sock, gpg)
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description="Blocks explorer")
+    parser.add_argument("node", metavar="<node>", help="Node name")
+    parser.add_argument("port", metavar="<port>", help="Node port")
+    parser.add_argument("--hash", nargs="?", help="block hash")
+    parser.add_argument("--height", nargs="?", help="block height")
+    args = parser.parse_args()
+    sock = socket(AF_INET, SOCK_DGRAM)
+    gpg = GPG()
+    block = None
+    if args.hash is not None:
+        block = explore_by_hash(args.hash, args.node, int(args.port), sock, gpg)
+    elif args.height is not None:
+        block = explore_by_height(int(args.height), args.node, int(args.port), sock, gpg)
+    print(block)
