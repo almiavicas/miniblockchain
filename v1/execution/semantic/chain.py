02bc4e5319c4076d008f98a00ae4124adc6ef41f
Alvaro Avila <aavila@codeforvenezuela.org>
2021-11-29 22:20:03 -0400

    corrected insert_block method for chain

--- v1/execution/semantic/chain.py
+++ v1/execution/semantic/chain.py
@@ -22,9 +22,17 @@ class Chain():
                 return deepcopy(block.transactions[tx_hash])
 
     def insert_block(self, block: Block):
-        self.blockchain[block._hash] = block
         for tx in block.transactions.values():
             tx.status = "CONFIRMED"
+            new_tx_input = []
+            for utxo in tx._input:
+                utxo_tx = self.find_tx_by_hash(utxo.tx_hash)
+                chain_utxo = utxo_tx.find_output_utxo(utxo.fingerprint_hash)
+                chain_utxo.spent = True
+                new_tx_input.append(chain_utxo)
+            tx._input = new_tx_input
+        self.blockchain[block._hash] = block
+                
 
     def last_block(self) -> Block:
         blocks = list(self.blockchain.values())
